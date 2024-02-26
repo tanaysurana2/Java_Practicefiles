3de018647383efd70892faecf3828e52525278c1
Tanaysurana2 <tanaysurana2@gmail.com>
2024-02-26 22:40:54 +0530

    First Change ever

--- src/Design_Patterns/Builder/Client.java
+++ src/Design_Patterns/Builder/Client.java
@@ -1,2 +1,8 @@
-package Design_Patterns.Builder;public class Client {
+package Design_Patterns.Builder;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("First Change");
+    }
 }
--- src/Design_Patterns/Singleton/Client.java
+++ src/Design_Patterns/Singleton/Client.java
@@ -1,2 +1,7 @@
-package Design_Patterns.Singleton;public class Client {
+package Design_Patterns.Singleton;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("First Change in Singleton");
+    }
 }
